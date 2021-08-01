62f0950f2715fba1a49b922b01ddfdf9f66b613f
Aleksei Saunders <alekseisaunders@gmail.com>
2021-07-31 18:26:58 -0600

    added ability to enter new products and save that to db

--- index.js
+++ index.js
@@ -5,31 +5,43 @@ const mongoose = require('mongoose');
 
 const Product = require('./models/product');
 
-
-mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-    console.log("mongo connection open!")
-})
-.catch(err => {
-    console.log('Mongo connection error')
-    console.log(err)
-})
+mongoose
+  .connect('mongodb://localhost:27017/farmStand', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('mongo connection open!');
+  })
+  .catch((err) => {
+    console.log('Mongo connection error');
+    console.log(err);
+  });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-  app.get('/products', async (req, res, next) => {
-    const products = await Product.find({})
-    res.render('products/index', {products})
- })
+app.get('/products', async (req, res, next) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+});
 
- app.get('/products/:id', async (req, res, next) => {
-    const {id} = req.params;
-    const product = await Product.findById(id);
-    console.log(product)
-    res.render('products/show', {product})
- })
+app.get('/products/new', (req, res, next) => {
+  res.render('products/new');
+});
 
+app.post('/products', async (req, res, next) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get('/products/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/show', { product });
+});
 
 app.listen(3000, () => {
   console.log('App is listening on PORT 3000');
